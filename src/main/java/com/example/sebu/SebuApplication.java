7b1a71e2c9ff19d88d4bebd99ef2a41d32aeb950
kwonkiju <rnjsrlwn2@naver.com>
2023-03-19 23:44:58 +0900

    commit

---
+++ src/main/java/com/example/sebu/SebuApplication.java
@@ -0,0 +1,13 @@
+package com.example.sebu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SebuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SebuApplication.class, args);
+	}
+
+}
