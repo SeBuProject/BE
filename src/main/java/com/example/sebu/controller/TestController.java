1c1cfb2a1212290a4e981d66330c8526f70864dc
kwonkiju <rnjsrlwn2@naver.com>
2023-03-20 00:31:18 +0900

    excel 읽기

--- src/main/java/com/example/sebu/controller/TestController.java
+++ src/main/java/com/example/sebu/controller/TestController.java
@@ -1,24 +1,64 @@
 package com.example.sebu.controller;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import jakarta.servlet.http.HttpServletRequest;
 @RestController
 public class TestController {
 
-		@GetMapping("/home")
-		public String goHome(HttpServletRequest request) {
-			Map map = new HashMap<>();
-			System.out.println("enter");
-			return "hello home";
-		}
+	  @PostMapping(value = "/home")
+	  public String readExcel(@RequestParam("file") MultipartFile file, Model model)
+	      throws IOException { // 2
+
+
+	    String extension = FilenameUtils.getExtension(file.getOriginalFilename()); // 3
+
+	    if (!extension.equals("xlsx") && !extension.equals("xls")) {
+	      throw new IOException("엑셀파일만 업로드 해주세요.");
+	    }
+
+	    Workbook workbook = null;
+
+	    if (extension.equals("xlsx")) {
+	      workbook = new XSSFWorkbook(file.getInputStream());
+	    } else if (extension.equals("xls")) {
+	      workbook = new HSSFWorkbook(file.getInputStream());
+	    }
+
+	     Sheet worksheet = workbook.getSheetAt(0);
+
+	    for (int i = 1; i < worksheet.getPhysicalNumberOfRows(); i++) { // 4
+
+	      Row row = worksheet.getRow(i);
+
+
+	      System.out.println(row.getCell(0).getStringCellValue());
+	      System.out.println(row.getCell(1).getStringCellValue());
+
+	    }
+
+	    return "excelList";
+
+	  }
+	}
 	
-}
+
