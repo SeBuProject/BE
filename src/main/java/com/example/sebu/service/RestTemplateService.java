cd22cf375a1fef1a5f6634e781923866feba6c2b
kwonkiju <rnjsrlwn2@naver.com>
2023-04-28 02:46:40 +0900

    Refactor: "다운로드 데이터 변환수정 및 데이터 빈값 유무 판단 메소드 추가"
    다운로드 데이터 변환 및 데이터 빈값 유무 판단 메소드 추가

--- src/main/java/com/example/sebu/service/RestTemplateService.java
+++ src/main/java/com/example/sebu/service/RestTemplateService.java
@@ -1,6 +1,8 @@
 package com.example.sebu.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -21,7 +23,12 @@ public class RestTemplateService {
 		JSONObject object = new JSONObject();
 		JSONArray businessStatusInquiryArr = new JSONArray();
 		int apiCallCnt = 1;
-		ArrayList<String> splitList = new ArrayList<String>();
+		//데이터 변환 변수
+        String b_no = "";
+        String end_dt = "";
+        String utcc_yn = "";
+        String tax_type_change_dt = "";
+        String invoice_apply_dt = "";
 		
 		//파라미터 가져옴
 		ArrayList<String> list = (ArrayList<String>)param;
@@ -51,6 +58,50 @@ public class RestTemplateService {
 			Utils.callbusinessStatusInquiryApi(url,object,businessStatusInquiryArr);
 		}
 		
+		//데이터 변환
+		for(int i=0; i<businessStatusInquiryArr.size(); i++) {
+			Map obj = (HashMap)businessStatusInquiryArr.get(i);
+			
+			b_no = String.valueOf(obj.get("b_no"));
+			//사업자번호 - 추가
+			StringBuilder sb = new StringBuilder();
+			sb.append(b_no);
+			b_no=sb.insert(3, "-").insert(6, "-").toString();
+			
+			
+			obj.put("b_no", b_no);
+			
+			end_dt = String.valueOf(obj.get("end_dt"));
+			if(Utils.isEmpty(end_dt)) {
+				if(end_dt.length() == 8) {
+					sb = new StringBuilder();
+					sb.append(end_dt);
+					obj.put("end_dt", sb.insert(4, "-").insert(7, "-"));
+				}
+			}
+	
+			utcc_yn = String.valueOf(obj.get("utcc_yn"));
+			
+			obj.put("utcc_yn", "Y".equals(utcc_yn) ? "여":"부");
+		
+			tax_type_change_dt = String.valueOf(obj.get("tax_type_change_dt"));
+			if(Utils.isEmpty(tax_type_change_dt)) {
+				if(tax_type_change_dt.length() == 8) {
+					sb = new StringBuilder();
+					sb.append(tax_type_change_dt);
+					obj.put("tax_type_change_dt", sb.insert(4, "-").insert(7, "-"));
+				}
+			}
+			
+			invoice_apply_dt = String.valueOf(obj.get("invoice_apply_dt"));
+			if(Utils.isEmpty(invoice_apply_dt)) {
+				if(invoice_apply_dt.length() == 8) {
+					sb = new StringBuilder();
+					sb.append(invoice_apply_dt);
+					obj.put("invoice_apply_dt", sb.insert(4, "-").insert(7, "-"));
+				}
+			}
+	}
 		resp.setData(businessStatusInquiryArr);
 		
 		return resp;
